1b8738749abacf22ce7c4ed7ffd02edc7bb1fb00
Manish Rawal <manish.rawal@convergetree.com>
2021-10-06 10:22:43 +0545

    'testing-initial-renders-of-ui-element-is-correct-or-not'

--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -4,8 +4,36 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("header renders with correct text", () => {
-  const component = render(<Counter />);
-  const headerEl = component.getByTestId("header");
+  const { getByTestId } = render(<Counter />);
+  const headerEl = getByTestId("header");
 
   expect(headerEl.textContent).toBe("My Counter");
+});
+
+test("counter initially start with text of 0", () => {
+  const { getByTestId } = render(<Counter />);
+  const counterEl = getByTestId("counter");
+
+  expect(counterEl.textContent).toBe("0");
+});
+
+test("input contains initial value of 1", () => {
+  const { getByTestId } = render(<Counter />);
+  const inputEl = getByTestId("input");
+
+  expect(inputEl.value).toBe("1");
+});
+
+test("add button renders with +", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+
+  expect(addBtn.textContent).toBe("+");
+});
+
+test("subtract button renders with -", () => {
+  const { getByTestId } = render(<Counter />);
+  const subtractBtn = getByTestId("subtract-btn");
+
+  expect(subtractBtn.textContent).toBe("-");
 });
